51e090ff1c0523f023f82ef4215f4ee9b712f949
s4llm4n <s4llm4n@gmail.com>
2025-03-13 14:45:21 +0700

    start toaster

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const { dbConnect } = require('./utils/db')
 
 app.use(cors({ 
     origin : ['http://localhost:3000'],
@@ -16,4 +17,5 @@ app.use('/api',require('./routes/authRoutes'))
 
 app.get('/',(req,res) => res.send('My backend'))
 const port = process.env.PORT
+dbConnect()
 app.listen(port, () => console.log(`Server is running on port ${port}`))
