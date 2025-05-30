455d191968d8c3d2ea753475d67b51dab9562c6b
s4llm4n <s4llm4n@gmail.com>
2025-05-30 15:30:39 +0700

    changeImage

--- controllers/dashboard/productController.js
+++ controllers/dashboard/productController.js
@@ -79,7 +79,70 @@ class productController{
             
         }
     }
+    // End Method
 
+    product_get = async (req, res) => {
+        const { productId } = req.params;
+        try {
+            const product = await productModel.findById(productId)
+            responseReturn(res, 200,{product})
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+    // End Methods
+
+    product_update = async (req, res) => {
+        let {name, description, stock, price, discount, brand, productId} = req.body;
+        name = name.trim()
+        const slug = name.split('').join('-')
+
+        try {
+            await productModel.findByIdAndUpdate(productId, {
+                name, description, stock, price, discount, brand, productId, slug
+            })
+            const product = await productModel.findById(productId)
+            responseReturn(res, 200,{product, message : 'Product Update Succesfully'})
+        } catch (error) {
+            responseReturn(res, 500,{ error : error.message })
+        }
+    }
+    // End Method
+
+    product_image_update = async(req,res) => {
+        const form = formidable({ multiples: true })
+
+        form.parse(req, async (err, field, files) => {
+            const {oldImage,productId} = field;
+            const { newImage } = files
+
+            if (err) {
+                responseReturn(res, 400,{ error : err.message })
+            } else {
+                try {
+
+                    cloudinary.config({
+                    cloud_name: process.env.cloud_name, 
+                    api_key : process.env.api_key, 
+                    api_secret : process.env.api_secret,
+                    secure : true
+                    })
+
+                    const result = await cloudinary.uploader.upload(newImage.filepath, { folder: 'products' })
+                    
+                    if (result) {
+                        let {images} = await productModel.findById(productId)
+                        
+                    } else {
+                        
+                    }
+
+                } catch (error) {
+                    
+                }
+            }
+        })
+    }
     // End Method
 
     }
