df65a2fe70450dfb4dcdb412aa39814e78fabb4b
s4llm4n <s4llm4n@gmail.com>
2025-06-21 22:35:13 +0700

    comitfetchallproduct

--- controllers/home/homeControllers.js
+++ controllers/home/homeControllers.js
@@ -1,7 +1,27 @@
 const categoryModel = require('../../models/categoryModel')
+const productModel = require('../../models/productModel')
 const { responseReturn } = require("../../utils/response")
 
 class homeControllers{
+
+    formateProduct = (products) => {
+        const productArray = [];
+        let i = 0;
+        while (i < products.length) {
+            let temp = []
+            let j = i 
+            while (j < i + 3) {
+                if (products[j]) {
+                    temp.push(products[j])
+                }
+                j++
+            }
+            productArray.push([...temp])
+            i = j
+        }
+        return productArray
+    }
+
     get_categorys = async(req,res) => {
         try {
             const categorys = await categoryModel.find({})
@@ -13,6 +33,43 @@ class homeControllers{
             console.log(error.message)
         }
     }
+    // End Method
+
+    get_products = async(req, res) => {
+        try {
+            const products = await productModel.find({}).limit(12).sort({
+                createdAt: -1
+            })
+            const allProduct1 = await productModel.find({}).limit(9).sort({
+                createdAt: -1
+            })
+            const latest_product = this.formateProduct(allProduct1);
+
+            const allProduct2 = await productModel.find({}).limit(9).sort({
+                rating: -1
+            })
+            const topRated_product = this.formateProduct(allProduct2);
+
+            const allProduct3 = await productModel.find({}).limit(9).sort({
+                discount: -1
+            })
+            const discount_product = this.formateProduct(allProduct3);
+
+
+            responseReturn(res, 200,{
+                products,
+                latest_product,
+                topRated_product,
+                discount_product
+            })
+
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+    // End Method
+
+
 }
 
 module.exports = new homeControllers()
