b594a1f6f014a0d1d7bca8cf2075f592b3207633
s4llm4n <s4llm4n@gmail.com>
2025-07-01 15:21:53 +0700

    cartfinish

--- controllers/home/cartController.js
+++ controllers/home/cartController.js
@@ -1,4 +1,5 @@
 const cartModel = require("../../models/cartModel")
+const { responseReturn } = require('../../utils/response')
 
 class cartController{
 
@@ -7,11 +8,31 @@ class cartController{
         try {
             const product = await cartModel.findOne({
                 $and: [{
-                    productId
-                }]
+                    productId : {
+                        $eq: productId
+                    }
+                },
+                {
+                    userId: {
+                        $eq: userId
+                    }
+                }
+            ]
             })
+
+            if (product) {
+                responseReturn(res,404,{error: 'Product Already Added to Cart'})
+            } else {
+                const product = await cartModel.create({
+                    userId,
+                    productId,
+                    quantity
+                })
+                responseReturn(res,201,{message: 'Product Added to Cart Successfully', product})
+            }
+
         } catch (error) {
-            
+            console.log(error.message)
         }
     }
     // End Method
