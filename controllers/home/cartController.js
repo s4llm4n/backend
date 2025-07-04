3a50791264c31dabceac12c1ce0b9662e41e86bd
s4llm4n <s4llm4n@gmail.com>
2025-07-04 10:10:37 +0700

    cartproc

--- controllers/home/cartController.js
+++ controllers/home/cartController.js
@@ -1,5 +1,6 @@
 const cartModel = require("../../models/cartModel")
 const { responseReturn } = require('../../utils/response')
+const { mongo: {ObjectId}} = require('mongoose')
 
 class cartController{
 
@@ -38,7 +39,50 @@ class cartController{
     // End Method
 
     get_cart_products = async(req, res) => {
-        console.log('helloo')
+        const {userId } = req.params
+        try {
+            const cart_products = await cartModel.aggregate([{
+                $match: {
+                    userId: {
+                        $eq: new ObjectId(userId)
+                    }
+                }
+            },
+        {
+            $lookup : {
+                from: 'products',
+                localField: 'productId',
+                foreignField: '_id',
+                as: 'products'
+            }
+        }
+        ])
+        let buy_product_item = 0
+        let calculatePrice = 0;
+        let cart_product_count = 0;
+        const outOfStockProduct = cart_products.filter(p => p.products[0].stock < p.quantity)
+        for (let i = 0; i < outOfStockProduct.length; i++) {
+            cart_product_count = cart_product_count + outOfStockProduct[i].quantity
+        }
+        const stockProduct = cart_products.filter(p => p.products[0].stock >= p.quantity)
+        for (let i = 0; i < stockProduct.length; i++) {
+            const { quantity } = stockProduct[i]
+            cart_product_count = buy_product_item + quantity
+
+            buy_product_item = buy_product_item + quantity
+            const { price,discount } = stockProduct[i].products[0]
+            if (discount !== 0) {
+                calculatePrice = calculatePrice + quantity * (price - Math.floor((price * discount) / 100))
+            } else {
+                calculatePrice = calculatePrice + quantity * price
+            }
+            
+        } // End for
+
+            console.log(buy_product_item)
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
 }
