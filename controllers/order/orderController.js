27c08eb409ba0815e1fca2b00a7b8616782e7057
s4llm4n <s4llm4n@gmail.com>
2025-07-08 13:43:29 +0700

    schemaorder

---
+++ controllers/order/orderController.js
@@ -0,0 +1,15 @@
+const moment = require("moment")
+
+class orderController{
+    place_order = async (req,res) => {
+        const {price,products,shipping_fee,shippingInfo,userId} = req.body
+        let authorOrderData = []
+        let cardId = []
+        const tempDate = moment(Date.now()).format('LLL')
+        console.log(tempDate)
+    }
+
+    // End Method
+}
+
+module.exports = new orderController()
