8181b9100c9970483ffb6db4030f244de5f7708e
s4llm4n <s4llm4n@gmail.com>
2025-07-09 11:53:26 +0700

    orderplaced

--- controllers/order/orderController.js
+++ controllers/order/orderController.js
@@ -1,12 +1,35 @@
 const authOrderModel = require('../../models/authOrder')
 const customerOrder = require('../../models/customerOrder') 
+const cartModel = require('../../models/cartModel')
 const moment = require("moment")
+const { responseReturn } = require('../../utils/response')
 
 class orderController{
+
+    paymentCheck = async (id) => {
+        try {
+            const order = await customerOrder.findById(id)
+            if (order.payment_status === 'unpaid') {
+                await customerOrder.findByIdAndUpdate(id, {
+                    delivery_status: 'cancelled'
+                })
+                await authOrderModel.updateMany({
+                    orderId: id
+                },{
+                    delivery_status: 'cancelled'
+                })
+            }
+            return true
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    // End Method
+
     place_order = async (req,res) => {
         const {price,products,shipping_fee,shippingInfo,userId} = req.body
         let authorOrderData = []
-        let cardId = []
+        let cartId = []
         const tempDate = moment(Date.now()).format('LLL')
 
         let customerOrderProduct = []
@@ -17,12 +40,59 @@ class orderController{
                 const tempCusPro = pro[j].productInfo;
                 tempCusPro.quantity = pro[j].quantity
                 customerOrderProduct.push(tempCusPro)
-                if (condition) {
-                    
+                if (pro[j]._id) {
+                    cartId.push(pro[j]._id)
                 }
-                // console.log(tempCusPro)
             }
         }
+
+        try {
+            const order = await customerOrder.create({
+                customerId: userId,shippingInfo,
+                products: customerOrderProduct,
+                price: price + shipping_fee,
+                payment_status : 'unpaid',
+                delivery_status : 'pending',
+                date: tempDate
+            })
+            for (let i = 0; i < products.length; i++) {
+                const pro = products[i].products;
+                const pri = products[i].price 
+                const sellerId = products[i].sellerId 
+                let storePor = [] 
+                for (let j = 0; j < pro.length; j++) {
+                    const tempPro = pro[j].productInfo;
+                    tempPro.quantity = pro[j].quantity 
+                    storePor.push(tempPro)
+                }
+
+                authorOrderData.push({
+                    orderId: order.id,sellerId,
+                    products: storePor,
+                    price:pri,
+                    payment_status: 'unpaid',
+                    shippingInfo: 'Easy Main Warehouse',
+                    delivery_status: 'pending',
+                    date: tempDate
+                })
+            } 
+
+            await authOrderModel.insertMany(authorOrderData)
+            for (let k = 0; k < cartId.length; k++) {
+                await cartModel.findByIdAndDelete(cartId[k]) 
+            }
+
+            setTimeout(() => {
+                this.paymentCheck(order.id)
+            }, 15000)
+
+            responseReturn(res,200,{message: 'Order Placed Success', orderId: order.id})
+
+        } catch (error) {
+            console.log(error.message)
+            
+        }
+
     }
     // End Method
 }
