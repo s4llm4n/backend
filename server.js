dfc3a2d6718971641cbaa2bfb6a954c8881a5cec
s4llm4n <s4llm4n@gmail.com>
2025-07-25 15:54:18 +0700

    socket create

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const app = express()
-require('dotenv').config()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const { dbConnect } = require('./utils/db')
 
+const socket = require('socket.io')
+const http = require('http')
+const server = http.createServer(app)
 app.use(cors({ 
     origin : ['http://localhost:3000'],
     credentials : true
 }))
+
+const io = socket(server, {
+    cors: {
+        origin: '*',
+        credentials: true
+    }
+})
+
+io.on('connection', (soc) => {
+    console.log('socket server running...')
+})
+
+require('dotenv').config()
+
 app.use(bodyParser.json())
 app.use(cookieParser())
 
@@ -26,4 +42,4 @@ app.use('/api',require('./routes/home/customerAuthRoutes'))
 app.get('/',(req,res) => res.send('Hello Server'))
 const port = process.env.PORT
 dbConnect()
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+server.listen(port, () => console.log(`Server is running on port ${port}`))
